daf811ebdd71497d4418ce58bd1e355a4fcc8407
Ron Haber <haberrj95@gmail.com>
2021-05-27 21:35:10 +0200

    Added player class and base structure

--- Support_Classes/player.py
+++ Support_Classes/player.py
@@ -3,3 +3,23 @@
 # Author: Ron Haber
 # Date: 25.05.2021
 # A class for creating each player and maintaining their attributes
+
+import os, sys
+import Support_Classes.card as card
+
+class Player(object):
+    def __init__(self, name):
+        self.hand = card.Pile()
+        self.face_down = card.Pile()
+        self.face_up = card.Pile()
+        self.name = name
+
+    def show_hand(self):
+        '''Will show the player their hand.
+        '''
+        cards = self.hand.getCards()
+        return cards
+    
+    def show_face_up(self):
+        cards = self.face_up.getCards()
+        return cards
